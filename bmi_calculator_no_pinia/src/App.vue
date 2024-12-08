f9d4f80f93439d3823dcae784b7ab5d49c3b7225
rosymaple <candace.vogel@go.minneapolis.edu>
2024-12-07 21:57:44 -0600

    Initial commit

---
+++ bmi_calculator_no_pinia/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import BodyMassIndexForm from './components/BodyMassIndexForm.vue'
+  import { ref } from 'vue'
+
+  // we need to declare a constant variable before...
+  // the calculateBmi function
+  const bmi = ref(0)
+
+  // must add weight to bmi computation
+  const calculateBMI = (height, weight) => {
+    // todo do calculation with weight
+    bmi.value = weight / (height * height)
+    return bmi.value
+    // return calculated bmi value to our program
+  }
+
+</script>
+
+<template>
+  <h1>Body Mass Index Calculator</h1>
+
+  <!-- todo set this up to expect a message
+  from BodyMassIndexForm and call a function here -->
+  <!-- must listen for user-entered-data event...
+  we must call the calculateBMI function if...
+  userHasSubmittedData gets triggered by the calculate button -->
+  <BodyMassIndexForm v-on:user-entered-data="calculateBMI" />
+  <!-- displays BMI for the user -->
+  <div>Your BMI is {{ bmi }}</div>
+</template>
+
+<style scoped>
+
+
+</style>
---
+++ bmi_calculator_no_pinia/src/components/BodyMassIndexForm.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from 'vue'
+
+const userHeight = ref('')
+const userWeight = ref('')
+
+const emit = defineEmits(['user-entered-data'])
+
+
+// todo create a function here to run when the user clicks a button
+// this function will emit a message to App.vue
+// including the user's height and weight data
+const userHasSubmittedData = () => {
+  // this function will be called when the user clicks the calculate button
+  // this component needs to tell App.vue that the data is available
+  // and send App.vue this data so it can do the math
+  emit ('user-entered-data', userHeight.value, userWeight.value) //todo include user weight
+}
+
+</script>
+
+<template>
+
+  <!-- HTML here, things the user sees -->
+  <!-- can be updated from the script's state -->
+  <!-- can tell the code the user has done something -->
+
+  <h2>Enter your height and weight</h2>
+
+  <label>Height in meters</label>
+  <input v-model="userHeight">
+  <br>
+
+  <!-- todo use v-model -->
+  <label>Weight in kilograms</label>
+  <input v-model="userWeight">
+  <br>
+
+  <!-- todo use v-on to tell the script
+  when the user clicks on the button -->
+  <button v-on:click="userHasSubmittedData">Calculate!</button>
+
+</template>
+
+<style scoped>
+
+</style>
